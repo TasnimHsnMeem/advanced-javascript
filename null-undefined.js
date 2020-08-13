2bf7f5a10e6aa6e3ad56025c01582f42d2027d9d
TasnimHsnMeem <tasnim.hsn.meem@gmail.com>
2020-08-13 06:38:34 -0400

    undefined and null cleared for interview

---
+++ null-undefined.js
@@ -0,0 +1,14 @@
+// let pakhi;
+// console.log(pakhi);
+// function add( num1, num2){
+//     console.log(num1 + num2);
+//     return;
+// }
+
+// function add( num1, num2){
+//     console.log(num1, num2);
+// }
+// const result = { name: "Kousin", Math:00, English:85}
+// console.log(result.bangla);
+let fun = undefined;
+console.log(fun);
